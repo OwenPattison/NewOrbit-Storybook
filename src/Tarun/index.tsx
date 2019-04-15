cd3ca927f2cfeb3f486bf3ac96f913f973cfdf64
Owen Pattison <opattison@neworbit.co.uk>
2019-04-15 08:48:53 +0100

    clean index

--- src/Tarun/index.tsx
+++ src/Tarun/index.tsx
@@ -1,13 +1,1 @@
 import * as React from "react";
-import { withKnobs } from '@storybook/addon-knobs';
-import { storiesOf } from "@storybook/react";
-
-const stories = storiesOf("Tarun", module)
-
-stories.addDecorator(withKnobs);
-
-stories
-    .add("Lovely Thing",() => (
-            <h1>Please add something lovely here!</h1>
-        ),
-    );
