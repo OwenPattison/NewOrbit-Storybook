9b2edebd9f5ca3a130519fa0e77ee498006fade1
James Monger <jameskmonger@hotmail.co.uk>
2019-04-26 10:13:37 +0100

    Monger: Create Button

--- stories/Monger/index.tsx
+++ stories/Monger/index.tsx
@@ -1,13 +1,17 @@
 import * as React from "react";
-import { withKnobs } from '@storybook/addon-knobs';
+import { withKnobs, text, select } from '@storybook/addon-knobs';
 import { storiesOf } from "@storybook/react";
+import { Button } from "../../src/Monger/Button/Button";
 
 const stories = storiesOf("Monger", module)
 
 stories.addDecorator(withKnobs);
 
 stories
-    .add("Lovely Thing",() => (
-            <h1>Please add something lovely here!</h1>
+    .add("Button",() => (
+            <Button type={select("Type", { Primary: "primary", Secondary: "secondary"}, "primary")}>{text("Text", "Click me")}</Button>
         ),
+        {
+            info: { inline: true }
+        }
     );
