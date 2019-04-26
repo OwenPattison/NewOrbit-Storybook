7076485ccc81fead1e405c8705d7838c49c6ab43
Stephen Rayner <srayner@neworbit.co.uk>
2019-04-26 10:54:35 +0100

    ugly component

--- stories/Stephen/index.tsx
+++ stories/Stephen/index.tsx
@@ -8,6 +8,38 @@ stories.addDecorator(withKnobs);
 
 stories
     .add("Lovely Thing",() => (
-            <h1>Please add something lovely here!</h1>
+            <div style={{
+                padding: "1em",
+                background: "linear-gradient(to right, red,orange,yellow,green,blue,indigo,violet)",
+                width: "100%",
+                height: "40vh",
+                cursor: "url(http://www.rw-designer.com/cursor-teaser/ultimate-unicorn.png), auto"
+
+            }}>
+                <h1 style={{
+                    textAlign: "center",
+                    fontSize: "3em",
+                    color: "purple",
+                }}>Hello world</h1>
+            </div>
         ),
+        {
+            info: { inline: true },
+            cssresources: [
+                {
+                    id: `neworbit`,
+                    code:
+                    `<style>
+                        .btn.primary {
+                            background-color: #FF9035;
+                        }
+
+                        .btn.primary:hover, .btn.primary:focus {
+                            background-color: #FF7A19;
+                        }
+                    </style>`,
+                    picked: false
+                },
+            ],
+        }
     );
