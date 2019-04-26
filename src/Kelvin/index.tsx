18c5e3aa28c186f9c940dfd588296c292c964ef1
Kelvin Stott <kstott@neworbit.co.uk>
2019-04-26 10:39:16 +0100

    Added breadcrumb

--- src/Kelvin/index.tsx
+++ src/Kelvin/index.tsx
@@ -1,1 +1,31 @@
-// import * as React from "react";
+import * as React from "react";
+import "./Breadcrumb.css";
+
+interface BreadcrumbProps {
+    steps: string[];
+    onClick: (step: string) => void;
+    activeColor: string;
+    inactiveColor: string;
+}
+
+const Breadcrumb = (props: BreadcrumbProps) => {
+    const [activeStep, setActiveStep] = React.useState(props.steps[0]);
+    return (
+    <div className="breadcrumb">
+        {props.steps.map((s, i) =>
+            <>
+                <a onClick={() => {
+                    props.onClick(s);
+                    setActiveStep(s);
+                }} key={s} className="breadcrumb-step" style={{
+                    color: s === activeStep ? props.activeColor : props.inactiveColor
+                }}>
+                    {s}
+                </a>
+                {props.steps.length-1 !== i && <div> > </div>}
+            </>
+        )}
+    </div>);
+};
+
+export { Breadcrumb, BreadcrumbProps };
